fa2629b08e7f73f328c5b3c5ee29ea75020626a9
Wayne Hockensmith <w.hockensmith@prodigy.net>
2016-10-22 21:04:32 -0500

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,18 @@
+## Read data set into memory
+dataset <- read.csv("household_power_consumption.txt",sep=";",header=TRUE)
+## Trim data down to 1 January 2007 to 2 February 2007
+data <- subset(dataset ,dataset$Date %in% c("1/2/2007","2/2/2007"))
+## Fix date and time
+data $Time <- strptime(paste(data$Date,data$Time,sep = " "), "%d/%m/%Y %H:%M:%S")
+data$Date <- strptime(data$Date,"%d/%m/%Y")
+## Fix/force other fields to numeric vaules
+data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
+data$Voltage <- as.numeric(as.character(data$Voltage))
+data$Global_reactive_power <- as.numeric(as.character(data$Global_reactive_power))
+data$Sub_metering_1 <- as.numeric(as.character(data$Sub_metering_1))
+data$Sub_metering_2 <- as.numeric(as.character(data$Sub_metering_2))
+data$Sub_metering_3 <- as.numeric(as.character(data$Sub_metering_3))
+## Draw histogram
+png("Plot1.png",width= 480, height = 480,  units= "px")
+hist(data$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
